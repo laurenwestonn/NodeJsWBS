3924770b181f63c18c4e6fac949afdd4efc7077c
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-28 13:46:45 +0100

    Add Mongoose and dotenv

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.send('Here\'s a GET route to "/"');
+	res.render('index');
 })
 
 module.exports = router;
