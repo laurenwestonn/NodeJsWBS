784e8e099e5b0c16b35271efd9e9003e6735fb70
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-29 21:16:25 +0100

    Front end form for new Books

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const BookModel = require('../models/book');
+const AuthorModel = require('../models/author');
 
 // All books 
 router.get('/', async (req, res) => {
@@ -8,8 +9,18 @@ router.get('/', async (req, res) => {
 })
 
 // New book
-router.get('/new', (req, res) => {
-	res.send('New book');
+router.get('/new', async (req, res) => {
+	try {
+		const authors = await AuthorModel.find({});
+		const book = new BookModel();
+		res.render('books/new', {
+			authors: authors,
+			book: book
+		});
+	} catch {
+		res.redirect('/books');
+	}
+	
 })
 
 // Create book
